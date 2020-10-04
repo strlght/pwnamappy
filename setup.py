11321a59935f6a43f388e76fb454237cbfa69da1
Grigorii Dzhanelidze <350804+strlght@users.noreply.github.com>
2020-10-04 16:08:48 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pwnamappy',
+    version='0.0.1',
+    description='Turn every pwn into pin',
+    author='Grigorii Dzhanelidze',
+    url='https://github.com/strlght/pwnamappy',
+    license='Apache License 2.0',
+    packages=find_packages()
+)
